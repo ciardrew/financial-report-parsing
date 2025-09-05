a637805d4542ceddc2578ebcd256ca0a3348edab
Ciaran Quigley <ciaran.quigley@stjohn.org.nz>
2025-09-05 12:52:23 +1200

    Formatting table headers

--- report_generator.py
+++ report_generator.py
@@ -7,12 +7,17 @@ def xlsx_create(df_dict):
         
         sheet_name = 'Combined Data'
         row_offset = 0
+
+        orange_format = writer.book.add_format({'bg_color': '#FFC000', 'bold': True, 'border': 1})
         
         for branch, df in df_dict.items():
             df.to_excel(writer, sheet_name=sheet_name, startrow=row_offset, index=False)
             working_sheet = writer.sheets[sheet_name]
             
-            working_sheet.write(row_offset, 0, branch)
+            for col_num, col_name in enumerate(df.columns):
+                working_sheet.write(row_offset, col_num, col_name, orange_format)
+
+            working_sheet.write(row_offset, 0, branch, orange_format)
 
             for col_num, column_name in enumerate(df.columns):
                 column_len = max(df[column_name].astype(str).map(len).max(), len(column_name))
